c95cbf8c2f8adcd60ff43d3d79a2df3e8294137c
Ian McCracken <ian.mccracken@gmail.com>
2014-01-09 11:02:44 -0600

    Update config file with comments.

--- main.go
+++ main.go
@@ -1,41 +1,52 @@
 package main
 
 import (
-	"flag"
+	"github.com/imdario/mergo"
 	"github.com/zenoss/glog"
+	flags "github.com/zenoss/go-flags"
 	"github.com/zenoss/metricshipper/lib"
+	"os"
 )
 
-var args struct {
-	redisUrl        string
-	readers         int
-	consumerUrl     string
-	writers         int
-	maxBufferSize   int
-	maxBatchSize    int
-	batchTimeout    float64
-	backoffWindow   int
-	maxBackoffSteps int
+var (
+	config *metricshipper.ShipperConfig
+)
+
+func parseShipperOpts(cfg *metricshipper.ShipperConfig) error {
+	parser := flags.NewParser(cfg, flags.Default|flags.IgnoreDefaults)
+	_, err := parser.Parse()
+	return err
+}
+
+func parseConfigFile(filename string, cfg *metricshipper.ShipperConfig) {
+	file, err := os.Open(filename)
+	if err == nil {
+		metricshipper.LoadConfig(file, cfg)
+	}
 }
 
 func init() {
-	flag.StringVar(&args.redisUrl, "redis-url", "redis://localhost:6379/0/metrics", "Redis URL to subscribe to")
-	flag.IntVar(&args.readers, "readers", 2, "Maximum number of simultaneous readers from Redis")
-	flag.StringVar(&args.consumerUrl, "consumer-url", "ws://localhost:9090/publish", "WebSocket URL of consumer to publish to")
-	flag.IntVar(&args.writers, "writers", 1, "Maximum number of simultaneous writers to the consumer")
-	flag.IntVar(&args.maxBufferSize, "max-buffer-size", 1024, "Maximum number of messages to keep in the internal buffer")
-	flag.IntVar(&args.maxBatchSize, "max-batch-size", 128, "Number of messages to send to the consumer in a single call. This should be smaller than the buffer size.")
-	flag.Float64Var(&args.batchTimeout, "batch-timeout", 0.1, "Maximum time in seconds to wait for messages from the internal buffer to be ready before making a web socket call with current metrics")
-	flag.IntVar(&args.backoffWindow, "backoff-window", 60, "Rolling time period in seconds to consider collision messages from the consumer")
-	flag.IntVar(&args.maxBackoffSteps, "max-backoff-steps", 16, "Maximum number of collisions to consider for exponential backoff")
-	flag.Parse()
+	defaults := &metricshipper.ShipperConfig{}
+	flags.ParseArgs(defaults, make([]string, 0))
+
+	config = &metricshipper.ShipperConfig{}
+	// Exit if help or parser error.
+	if err := parseShipperOpts(config); err != nil {
+		os.Exit(1)
+	}
+
+	fileconfig := &metricshipper.ShipperConfig{}
+	parseConfigFile(config.ConfigFilePath, fileconfig)
+
+	mergo.Merge(config, *fileconfig)
+	mergo.Merge(config, *defaults)
 }
 
 func main() {
 	glog.Info("Initiating 1 connection to consumer")
 	// First, connect to the websocket
-	w, err := metricshipper.NewWebsocketPublisher(args.consumerUrl,
-		args.readers, args.maxBufferSize, args.maxBatchSize)
+	w, err := metricshipper.NewWebsocketPublisher(config.ConsumerUrl,
+		config.Readers, config.MaxBufferSize, config.MaxBatchSize)
 	if err != nil {
 		glog.Fatal("Unable to create WebSocket forwarder")
 		return
@@ -47,7 +58,7 @@ func main() {
 	}
 	// Next, try to talk to redis
 	glog.Info("Initiating 2 connections to redis")
-	r, err := metricshipper.NewRedisReader(args.redisUrl, 128, 1024, 1)
+	r, err := metricshipper.NewRedisReader(config.RedisUrl, 128, 1024, 1)
 	if err != nil {
 		glog.Fatal("Unable to create redis reader")
 		return
