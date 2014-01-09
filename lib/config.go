6a18cddc5dc0b04a2e09ec2ede01732b304fcc7b
Ian McCracken <ian.mccracken@gmail.com>
2014-01-09 13:28:40 -0600

    Finish up config

---
+++ lib/config.go
@@ -0,0 +1,65 @@
+package metricshipper
+
+import (
+	"github.com/imdario/mergo"
+	flags "github.com/zenoss/go-flags"
+	"io"
+	"io/ioutil"
+	yaml "launchpad.net/goyaml"
+	"os"
+)
+
+type ShipperConfig struct {
+	ConfigFilePath  string  `long:"config" short:"c" description:"Path to configuration file"`
+	RedisUrl        string  `long:"redis-url" description:"Redis URL to subscribe to" default:"redis://localhost:6379/0/metrics"`
+	Readers         int     `long:"readers" description:"Maximum number of simultaneous readers from Redis" default:"2"`
+	ConsumerUrl     string  `long:"consumer-url" description:"WebSocket URL of consumer to publish to" default:"ws://localhost:8080/ws/metrics/store"`
+	Writers         int     `long:"writers" description:"Maximum number of simultaneous writers to the consumer" default:"1"`
+	MaxBufferSize   int     `long:"max-buffer-size" description:"Maximum number of messages to keep in the internal buffer" default:"1024"`
+	MaxBatchSize    int     `long:"max-batch-size" description:"Number of messages to send to the consumer in a single web socket call. This should be smaller than the buffer size." default:"128"`
+	BatchTimeout    float64 `long:"batch-timeout-seconds" description:"Maximum time in seconds to wait for messages from the internal buffer to be ready before making a web socket call with current metrics." default:"0.1"`
+	BackoffWindow   int     `long:"backoff-window-seconds" description:"Rolling time period in seconds to consider collision messages from the consumer." default:"60"`
+	MaxBackoffSteps int     `long:"max-backoff-steps" description:"Maximum number of collisions to consider for exponential backoff." default:"16"`
+	Username        string  `long:"username" description:"Username to use when connecting to the consumer"`
+	Password        string  `long:"password" description:"Password to use when connecting to the consumer"`
+}
+
+func LoadYAMLConfig(reader io.Reader, cfg *ShipperConfig) error {
+	bytes, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return err
+	}
+	err = yaml.Unmarshal(bytes, cfg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ParseShipperConfig() (*ShipperConfig, error) {
+	defaultopts := &ShipperConfig{}
+	cfgfileopts := &ShipperConfig{}
+	commandlineopts := &ShipperConfig{}
+
+	// Parse the options with no arguments to get defaults
+	flags.ParseArgs(defaultopts, make([]string, 0))
+
+	// Parse command-line options with no defaults
+	parser := flags.NewParser(commandlineopts, flags.Default|flags.IgnoreDefaults)
+	if _, err := parser.Parse(); err != nil {
+		return nil, err
+	}
+
+	// Now that we have the config file (if passed in), parse that
+	file, err := os.Open(commandlineopts.ConfigFilePath)
+	if err == nil {
+		LoadYAMLConfig(file, cfgfileopts)
+	}
+
+	// Replace zero-value entries in command-line opts with config file values
+	mergo.Merge(commandlineopts, *cfgfileopts)
+	// Replace any remaining zero-value entries with defaults
+	mergo.Merge(commandlineopts, *defaultopts)
+
+	return commandlineopts, nil
+}
--- main.go
+++ main.go
@@ -1,48 +1,17 @@
 package main
 
 import (
-	"github.com/imdario/mergo"
 	"github.com/zenoss/glog"
-	flags "github.com/zenoss/go-flags"
 	"github.com/zenoss/metricshipper/lib"
 	"os"
 )
 
-var (
-	config *metricshipper.ShipperConfig
-)
-
-func parseShipperOpts(cfg *metricshipper.ShipperConfig) error {
-	parser := flags.NewParser(cfg, flags.Default|flags.IgnoreDefaults)
-	_, err := parser.Parse()
-	return err
-}
-
-func parseConfigFile(filename string, cfg *metricshipper.ShipperConfig) {
-	file, err := os.Open(filename)
-	if err == nil {
-		metricshipper.LoadConfig(file, cfg)
-	}
-}
-
-func init() {
-	defaults := &metricshipper.ShipperConfig{}
-	flags.ParseArgs(defaults, make([]string, 0))
-
-	config = &metricshipper.ShipperConfig{}
-	// Exit if help or parser error.
-	if err := parseShipperOpts(config); err != nil {
+func main() {
+	config, err := metricshipper.ParseShipperConfig()
+	if err != nil {
 		os.Exit(1)
 	}
 
-	fileconfig := &metricshipper.ShipperConfig{}
-	parseConfigFile(config.ConfigFilePath, fileconfig)
-
-	mergo.Merge(config, *fileconfig)
-	mergo.Merge(config, *defaults)
-}
-
-func main() {
 	glog.Info("Initiating 1 connection to consumer")
 	// First, connect to the websocket
 	w, err := metricshipper.NewWebsocketPublisher(config.ConsumerUrl,
